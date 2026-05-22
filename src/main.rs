0ded0064e0ee0c9e1418525f4d85266760e3242e
xvs <osenberg@qq.com>
2026-05-22 23:19:14 +0800

    added more code

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,48 @@ struct FpsCounter {
 }
 
 fn main() {
-    App::new()
+    (App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         // .add_systems(Update, counter_fps)
         .add_systems(FixedUpdate, counter_fps)
-        .run();
+        .add_systems(Update, asset_system))
+    .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands.spawn_empty().insert(FpsCounter::default());
+    // 立方体
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+    // 灯光
+    commands.spawn((
+        PointLight {
+            shadow_maps_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    // 相机
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
+
+fn asset_system(time: Res<Time>) {
+    println!(
+        "this how I see time: delta {:?}, elapsed: {:?}",
+        time.delta(),
+        time.elapsed()
+    )
 }
 
 fn counter_fps(mut counters: Query<&mut FpsCounter>) {
